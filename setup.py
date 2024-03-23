e86ae195a773cd781bdba19901c818318cb64a00
Ankur Das <dankur542@gmail.com>
2024-03-23 16:10:31 +0530

    Setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="Onkur7",
+    author_email="dankur542@gmail.com",
+    packages=find_packages()
+)
